33bbf566fccc51ea4268eaebf4629c9ad0d16ad4
Nick Leon <nicolas7@vt.edu>
2021-10-21 18:32:12 -0400

    Updates

--- react-portfolio/src/components/AboutMe.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Title from './animations/title'
-const text = "About me"
-
-function AboutMe() {
-    return <Title text={text}/>
-}
-
-export default AboutMe
--- react-portfolio/src/components/animations/title.js
+++
@@ -1,6 +0,0 @@
-import { useSpring, animated } from 'react-spring'
-
-export default function Title(text) {
-  const props = useSpring({ to: { opacity: 1, y:10 }, from: { opacity: 0, y:0 } })
-  return <animated.h1 style={props}>{text.text}</animated.h1>
-}
